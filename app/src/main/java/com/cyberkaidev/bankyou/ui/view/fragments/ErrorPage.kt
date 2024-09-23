73fc7e590cf33c1d01d0cbea7ebd0b2ccedf8777
cyberkaidev <cyberkaidev@gmail.com>
2024-09-22 22:29:27 -0300

    :sparkles: ErrorPage created

---
+++ app/src/main/java/com/cyberkaidev/bankyou/ui/view/fragments/ErrorPage.kt
@@ -0,0 +1,31 @@
+package com.cyberkaidev.bankyou.ui.view.fragments
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.cyberkaidev.bankyou.ui.view.shared.ButtonView
+
+@Composable
+fun ErrorPage(onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.padding(top = 16.dp).fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Ops, an internal error occurred",
+            modifier = Modifier.padding(bottom = 10.dp),
+            style = TextStyle(textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
+        )
+        ButtonView(onClick = onClick) {
+            Text("Try again")
+        }
+    }
+}
