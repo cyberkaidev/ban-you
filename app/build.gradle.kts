be9aa76ec1b78c3785522580b6ebb623b1923033
cyberkaidev <cyberkaidev@gmail.com>
2024-09-22 23:15:44 -0300

    :bookmark: versionCode and versionName changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.cyberkaidev.bankyou"
         minSdk = 27
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
